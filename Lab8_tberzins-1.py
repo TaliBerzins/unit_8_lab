b9c4ed61cb1913a73f3ca8903615548559676cb7
TaliBerzins <tali.berzins@outlook.com>
2026-03-04 13:33:58 -0500

    Setting up initial function, making sure it works for just UPC number

---
+++ Lab8_tberzins-1.py
@@ -0,0 +1,12 @@
+"""UPC Validator
+Tali Berzins
+To validate a 12-digit UPC-A code
+03/04/2026"""
+
+upc_number = input("Enter a 12-digit UPC number")
+upc_input = upc_number[:-1]
+def find_UPC(upc_input: str):
+    print(upc_input)
+
+
+find_UPC(upc_input)
