88dcba28fa2a63314293093cd1c0d90eadea7187
TaliBerzins <tali.berzins@outlook.com>
2026-03-04 16:36:35 -0500

    Got the rest of the code setup to match the lab's output but I have a bug where I am getting the wrong check digit

--- Lab8_tberzins-1.py
+++ Lab8_tberzins-1.py
@@ -3,28 +3,40 @@ Tali Berzins
 To validate a 12-digit UPC-A code
 03/04/2026"""
 #Asking user for UPC number then outputting the first 11 digits and the check digit
-upc_number = input("Enter a 12-digit UPC number")
+upc_number = input("Enter a 12-digit UPC number\n")
 upc_input = upc_number[:-1]
-print(f'The first 11 digits are {upc_input}')
-print(f'The provided check digit is {upc_number[-1]}')
+print(f'The first 11 digits are {upc_input}.')
+print(f'The provided check digit is {upc_number[-1]}.\n')
 
 #Defining function to check expected check digit
 def find_UPC(upc_input: str):
     print("Calculating...")
     total = 0
+#For loop to run through the first 11 digits of the UPC and perform the check
     for i in range(len(upc_input)):
-        print(i)
-        input_digit = int(upc_input[i])
-        print(input_digit)
-#Checking if digit is at an odd or even position, note odds are actually evens because index starts at zero rather than 1 
+        input_digit = int(upc_input[i])     
+#Checking if digit is at an odd or even position, note odds are actually evens in context of the actual UPC number because index starts at zero rather than 1 
         if(i%2 ==0):
             total += input_digit
+            
         else:
             total += (input_digit*3)
+          
         i +=1 
     
-    print(total)
+    #Calculating the expected last digit and returning it
+    result_modulo: int = total%10
+    result: int = 10 - result_modulo
+    print(f"The expected check digit is {result}.\n")
+    return result
+
 
         
+#If statement to write output depending if last digit is valid or not
+if find_UPC(upc_input) == upc_number[-1]:
+    print("UPC is VALID")
+else:
+    print("UPC is INVALID")
 
-find_UPC(upc_input)
+
+
